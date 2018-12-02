341b470dcff0293a56270bc7df400dba6cb6d790
Florent Chehab <florent.chehab@gmail.com>
2018-12-02 22:15:27 +0100

    Quick link to new data pipeline and dependencies added

--- app/views/graph/Graph.js
+++ app/views/graph/Graph.js
@@ -1,17 +1,20 @@
 import cytoscape from "cytoscape";
-import _ from "lodash";
+// import _ from "lodash";
 import dataManagerInstance from "../../fetchData/DataManager";
+// import { runInThisContext } from "vm";
 
 class Graph {
   constructor(context) {
-    this.context     = context;
-    this.rawData     = Array();
+    this.context = context;
+    this.rawData = Array();
     this.connections = {};
-    this.nodes       = Array();
-    this.edges       = Array();
+    this.nodes = Array();
+    this.edges = Array();
   }
 
   init() {
+    dataManagerInstance.subscribe(this);
+
     this.cy = cytoscape({
       container: this.context,
       style: [
@@ -35,91 +38,119 @@ class Graph {
       layout: {
         name: "grid",
         rows: 1
-      }});
-
-    const bunchOfData = dataManagerInstance.getSampleData(1);
-    Promise.all(bunchOfData)
-      .then((allData) => {
-        allData.forEach(part => this.appendData(part.data));
-        this.processData();
-        this.updateViz();
-      });
-  }
-
-  appendData(data) {
-    this.rawData.push.apply(this.rawData, data);
-  }
-
-  processData() {
-    const bySource = _(this.rawData)
-      .chain()
-      .groupBy("mentionSourceName")
-      .value();
-
-    const byEvent  = _(this.rawData)
-      .chain()
-      .groupBy("eventId")
-      .value();
-
-    _.forEach(Object.keys(bySource), (lkey) => {
-      this.connections[lkey] = {};
+      }
     });
 
-    _.forEach(Object.keys(bySource), (lkey) => {
-      _.forEach(bySource[lkey],(lmen) => {
-        const rmentions = _.filter(byEvent[lmen.eventId],e=>e.mentionSourceName > lkey);
-        _.forEach(rmentions, (rmen) => {
-          this.connections[lmen.mentionSourceName][rmen.mentionSourceName] =
-              (this.connections[lmen.mentionSourceName][rmen.mentionSourceName]) || [];
-          this.connections[rmen.mentionSourceName][lmen.mentionSourceName] =
-              (this.connections[rmen.mentionSourceName][lmen.mentionSourceName]) || [];
+  }
 
-          this.connections[lmen.mentionSourceName][rmen.mentionSourceName].push(rmen);
-          this.connections[rmen.mentionSourceName][lmen.mentionSourceName].push(rmen);
-        });
-      });
-    });
+  updateData(data) {
+    const { graph } = data;
+    this.processData(graph["mappingIdSource"], graph["data"]);
+    this.updateViz();
+  }
+  // appendData(data) {
+  //   this.rawData.push.apply(this.rawData, data);
+  // }
 
-    this.nodes = _(this.connections)
-      .chain()
-      .map((neighborhood,node) => ({id: node}))
-      .value();
+  processData(sources, info) {
+    // const bySource = _(this.rawData)
+    //   .chain()
+    //   .groupBy("mentionSourceName")
+    //   .value();
 
-    this.edges = _(this.connections)
-      .chain()
-      .map((neighborhood,node) => {
-        return _.map(neighborhood,(mentions,name) => {
-          return ({
-            id:     node+"_"+name,
-            source: node,
-            target: name
-          });
-        });
-      })
-      .flatten()
-      .value();
+    // const byEvent  = _(this.rawData)
+    //   .chain()
+    //   .groupBy("eventId")
+    //   .value();
+
+    // _.forEach(Object.keys(bySource), (lkey) => {
+    //   this.connections[lkey] = {};
+    // });
+
+    // _.forEach(Object.keys(bySource), (lkey) => {
+    //   _.forEach(bySource[lkey],(lmen) => {
+    //     const rmentions = _.filter(byEvent[lmen.eventId],e=>e.mentionSourceName > lkey);
+    //     _.forEach(rmentions, (rmen) => {
+    //       this.connections[lmen.mentionSourceName][rmen.mentionSourceName] =
+    //           (this.connections[lmen.mentionSourceName][rmen.mentionSourceName]) || [];
+    //       this.connections[rmen.mentionSourceName][lmen.mentionSourceName] =
+    //           (this.connections[rmen.mentionSourceName][lmen.mentionSourceName]) || [];
+
+    //       this.connections[lmen.mentionSourceName][rmen.mentionSourceName].push(rmen);
+    //       this.connections[rmen.mentionSourceName][lmen.mentionSourceName].push(rmen);
+    //     });
+    //   });
+    // });
+
+    this.nodes = [];
+    for (const key in sources) {
+      this.nodes.push({ id: key });
+    }
+
+    this.edges = [];
+    for (const eventId in info) {
+      // console.log(this.edges.length)
+      if (this.edges.length > 10000) break; // TODO better
+      for (const source1 in info[eventId]) {
+        if (this.edges.length > 10000) break; // TODO better
+        for (const source2 in info[eventId]) {
+          if (source1 != source2) {
+            this.edges.push({
+              id: eventId + source1 + source2,
+              source: source1,
+              target: source2
+            });
+          }
+        }
+      }
+    }
+
+    // console.log(this.edges);
+    // this.nodes = _(this.connections)
+    //   .chain()
+    //   .map((neighborhood, node) => ({ id: node }))
+    //   .value();
+
+    // this.edges = _(this.connections)
+    //   .chain()
+    //   .map((neighborhood, node) => {
+    //     return _.map(neighborhood, (mentions, name) => {
+    //       return ({
+    //         id: node + "_" + name,
+    //         source: node,
+    //         target: name
+    //       });
+    //     });
+    //   })
+    //   .flatten()
+    //   .value();
   }
 
   updateViz() {
     this.cy.elements().remove();
 
-    const cy_nodes = _(this.nodes)
-      .map(n => ({group: "nodes", data:n}))
-      .value();
+    const cy_nodes = this.nodes.map(data => {
+      return { group: "nodes", data };
+    });
 
-    const cy_edges = _(this.edges)
-      .map(e => ({group: "edges", data:e}))
-      .value();
+    const cy_edges = this.edges.map(
+      data => {
+        return { group: "edges", data };
+      });
+
+    // const cy_edges = _(this.edges)
+    //   .map(e => ({ group: "edges", data: e }))
+    //   .value();
 
     this.cy.add(cy_nodes);
     this.cy.add(cy_edges);
 
-    console.log(cy_edges);
-    console.log(cy_nodes);
+    //console.log(cy_edges);
+    //console.log(cy_nodes);
 
     this.cy.layout({
-      name:"concentric",
-      nodeOverlap:20,
+      name: "concentric",
+      nodeOverlap: 20,
       fit: true,
       randomize: false,
       componentSpacing: 100,
