b1f0cfb926e21055e892452c56b3eb93e84bc0b1
striantafyllouEPFL <efstratios.triantafyllou@epfl.ch>
2018-12-12 01:38:47 +0100

    Fix animation bugs

--- app/views/stackedGraph/StackedGraph.js
+++ app/views/stackedGraph/StackedGraph.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import dataManagerInstance from "../../fetchData/DataManager";
 import "../../../assets/styles/StackedGraph.scss";
-//import _ from "lodash";
+import { shuffle } from "lodash";
 
 class StackedGraph {
   constructor(context) {
@@ -17,8 +17,8 @@ class StackedGraph {
 
     // Data for the active view of the graph (Streamgraph/Drilldown)
     this.active = {
-      data: Array(),
-      layerIDs: Array()
+      layerIDs: Array(),
+      nullStack: ""
     };
   }
 
@@ -40,88 +40,98 @@ class StackedGraph {
   }
 
   updateData(data) {
+    // Handle incoming data
     const update = data.stackedGraph;
     update.dates = update.dates.map((d) => new Date(d));
-    Object.assign(this.data, update);
-    this.active.data = this.data.streamgraph;
+
+    // Update view data
+    this.data = update;
+
+    // Update viz data
     this.active.layerIDs = (() => {
       const ids = Object.keys(update.streamgraph[0]);
       ids.pop("mentionInterval");
       return ids;
     })();
+    this.active.nullStack = (() => d3.stack().keys(["nullArea"])(
+      this.data.dates.map((d) => ({mentionInterval: d, nullArea: true})))[0]
+    )();
+
+    // Update viz
     this.generateStreamgraph();
   }
 
   generateStreamgraph() {
-    const data = this.data.streamgraph;
-    const chroma = (d, i) => d3.interpolateSpectral(
-      i / this.active.layerIDs.length
-    );
-    const stack = d3.stack()
-      .keys(this.active.layerIDs)
-      .order(d3.stackOrderInsideOut)
-      .offset(d3.stackOffsetWiggle);
-
-    this.updateViz(data, chroma, stack)
-      .on("click", (d) => this.generateDrilldown(d.key));
+    this.updateViz(
+      this.data.streamgraph,
+      (d, i) => d3.interpolateYlGnBu(i / this.active.layerIDs.length),
+      d3.stack()
+        .keys(this.active.layerIDs)
+        .order(d3.stackOrderInsideOut)
+        .offset(d3.stackOffsetWiggle)
+    ).on("click", (d) => this.generateDrilldown(d.key));
   }
 
   generateDrilldown(k) {
-    const data = this.data.drilldown[k];
-    const chroma = (d) => d.key ? d3.color("green") : d3.color("red");
-    const stack = d3.stack()
-      .keys([true, false])
-      .value((d, k) => k ? d[k] : -Math.max(0.0001, d[k]))
-      .order(d3.stackOrderNone)
-      .offset(d3.stackOffsetDiverging);
-
-    this.updateViz(data, chroma, stack)
-      .on("click", null);
+    this.updateViz(
+      this.data.drilldown[k],
+      (d) => d.key ? d3.rgb(0, 168, 107) : d3.rgb(185, 14, 10),
+      d3.stack()
+        .keys([true, false])
+        .value((d, k) => k ? d[k] : -Math.max(0.0001, d[k]))
+        .order(d3.stackOrderNone)
+        .offset(d3.stackOffsetDiverging)
+    ).on("click", null);
   }
 
   updateViz(data, chroma, stack) {
-    const firstDate = this.data.dates[0],
-      lastDate = this.data.dates[1];
+    const graphData = stack(data);
 
     const x = d3.scaleTime()
-      .domain([firstDate, lastDate])
+      .domain([d3.min(this.data.dates), d3.max(this.data.dates)])
       .range([0, 1000]);
-
     const y = d3.scaleLinear()
+      .domain([
+        d3.min(graphData, g => d3.min(g, d => d[0])),
+        d3.max(graphData, g => d3.max(g, d => d[1]))
+      ])
       .range([400, 0]);
 
     const area = d3.area()
       .curve(d3.curveBasis)
       .x((d) => x(new Date(d.data.mentionInterval)))
-      .y((d) => y(d[0]))
-      .y1((d) => y(d[1]));
+      .y0((d) => d.data.nullArea ? y(0) : y(d[0]))
+      .y1((d) => d.data.nullArea ? y(0) : y(d[1]));
+    const nullArea = area(this.active.nullStack);
 
-    const graphData = stack(data);
-    y.domain([
-      d3.min(graphData, g => d3.min(g, d => d[0])),
-      d3.max(graphData, g => d3.max(g, d => d[1]))
-    ]);
-
-    const tooltip = d3.select(this.context)
-      .append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
-    const u = this.chart.selectAll("path")
-      .data(graphData, (d) => d.index);
-
-    u.enter().append("path")
-        .attr("class", "stackedLayer")
-        .attr("d", "")
-        .attr("opacity", 1)
-      .merge(u)
-        .transition()
-        .duration(900)
+    // Set up animations
+    function animateLayers(path, duration, area, opacity) {
+      path.transition("animateLayers")
+        .duration(duration)
         .ease(d3.easeQuadOut)
         .attr("d", area)
-        .attr("fill", (d, i) => chroma(d, i));
+        .attr("opacity", opacity);
+    }
 
-    u.exit().remove();
+    function fadeLayers(path, duration, opacity) {
+      path.transition("hoverOnLayers")
+        .duration(200)
+        .attr("opacity", opacity);
+    }
+
+    const u = this.chart.selectAll("path")
+      .data(graphData);
+    u.enter().append("path")
+        .attr("class", "stackedLayer")
+        .attr("d", nullArea)
+        .attr("opacity", 0)
+      .merge(u)
+        .attr("fill", (d, i) => chroma(d, i))
+        .call(animateLayers, 1200, area, 1);
+
+    u.exit()
+      .call(animateLayers, 1200, nullArea, 0)
+      .on("end", (d) => d.remove());
 
     this.axis
       .transition()
@@ -132,28 +142,12 @@ class StackedGraph {
     const layers = this.chart.selectAll(".stackedLayer");
     layers
       .on("mouseover", (d, i) => {
-        layers.transition()
-          .duration(200)
-          .attr("opacity", (d, j) => j != i ? 0.6 : 1)
-          .style("cursor", "pointer");
-        tooltip.transition()
-          .duration(200)
-          .style("opacity", .9);
-        tooltip.html(d.key)
-          .style("left", (d3.event.pageX) + "px")
-          .style("top", (d3.event.pageY-30) + "px");
+        layers.style("cursor", "pointer")
+          .call(fadeLayers, 200, (d, j) => j != i ? 0.6 : 1);
       })
       .on("mouseout", () => {
-        layers.transition()
-          .duration(200)
-          .attr("opacity", 1)
-          .style("cursor", "default");
-        tooltip.transition()
-          .duration(200)
-          .style("opacity", 0);
-        tooltip.html("")
-          .style("left", "0px")
-          .style("top", "0px");
+        layers.style("cursor", "default")
+          .call(fadeLayers, 200, 1);
       });
     return layers;
   }
