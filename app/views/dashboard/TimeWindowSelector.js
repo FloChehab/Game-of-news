cb313c016150de6546bab5dbe4562e4f995baccf
striantafyllouEPFL <efstratios.triantafyllou@epfl.ch>
2018-12-20 21:19:04 +0100

    Make time window selector responsive

--- app/views/dashboard/TimeWindowSelector.js
+++ app/views/dashboard/TimeWindowSelector.js
@@ -44,8 +44,10 @@ class TimeWindowSelector {
     this.brushGroup = false;
 
     this.svg = d3.select(DIVID).append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      //.attr("width", width + margin.left + margin.right)
+      //.attr("height", height + margin.top + margin.bottom)
+      .attr("viewBox", `0, 0, ${width + margin.left + margin.right}, ${height + margin.top + margin.bottom}`)
+      .attr("preserveAspectRatio", "xMinYMin meet")
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
