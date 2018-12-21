679580db566373f09fcae0f204f21f88c0a6d6df
Florent Chehab <florent.chehab@gmail.com>
2018-12-21 23:38:04 +0100

    Added datasets

--- app/config.js
+++ app/config.js
@@ -8,7 +8,13 @@ let CONFIG = {
   // For simplicity we set the max querrying time to 24 houts earlier
   LAST_AVAILABLE_GDELT_DATETIME: twoDaysBefore,
   MAX_NB_HOURS_GBQ_QUERY: 24,
-  PRE_FETCHED_DATASETS: ["gilet_jaune_10th_December_->_11th_December.json"],
+  PRE_FETCHED_DATASETS: [
+    "gilet_jaune_10th_December_->_11th_December.json",
+    "gilet_jaune_30th_November_->_2nd_December.json",
+    "gilet_jaune_7th_December_->_8th_December.json",
+    "Donald_Trump_election_8th_November_2016_->_9th_November_2016.json",
+    "Khashoggi_case_20th_October_2018_->_21th_October_2018.json"
+  ],
   END_POINT_LIVE_GDELT_DATA: "http://0.0.0.0:8000/",
   //"END_POINT_LIVE_GDELT_DATA": LIVE_API,
   GRAPH_MAX_NB_NODES: 40,
